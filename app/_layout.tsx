da1f4813192e7fe8bef2e4fd4b99af6166c64fe3
maher-alhomsy <maheralhomsy2002@gmail.com>
2025-02-03 04:04:44 +0300

    remove tabs screen, create index screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,24 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
 
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { tokenCache } from '@/utils/cache';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+const InitialLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
 
 export default function RootLayout() {
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-  if (!publishableKey) {
-    throw new Error(
-      'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
-    );
-  }
-
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <ClerkLoaded>
-        <ThemeProvider
-          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
+        <InitialLayout />
       </ClerkLoaded>
     </ClerkProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Page = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Page</Text>
+    </View>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({});
